39b8bedc7d4cc708256f36cc3053b6d39a92ef4e
jimlbeaver <home@jimbeaver.com>
2023-07-06 12:58:14 -0700

    changed to work on the remote desktop server

--- python.bat
+++ python.bat
@@ -1,8 +1,9 @@
 
 @echo off
-REM C:\Program Files\Thermo-Calc\2023a\python\python.exe %1
+REM This is the one to use on the box
+C:\Program Files\Thermo-Calc\2021a\python\python.exe "%~1" %*
 
-REM Run the Python script with the passed arguments
-"C:\Users\live\AppData\Local\Programs\Python\Python39\python.exe" "%~1" %*
+REM This is the one to use to test on your local machine...in cmd prompt type "where python" to find the path to your python.exe
+REM "C:\Users\live\AppData\Local\Programs\Python\Python39\python.exe" "%~1" %*
 
 pause
