79fd419b0dac935181bff5870385890f398667df
jimlbeaver <home@jimbeaver.com>
2023-07-07 16:39:37 -0700

    dynamically build the table

--- format_results.py
+++ format_results.py
@@ -15,7 +15,6 @@ class Results():
         return self.label
 
 
-
 def x_table(results_dict):
     x_table = {}
     all_x_values = set(x for value in results_dict.values() for x in value.get_x())
@@ -31,31 +30,30 @@ def x_table(results_dict):
     x_table = {k: v for k, v in sorted(x_table.items(), key=lambda item: item[0])}
     return x_table
 
-def format_table(x_table):
+
+def format_table(x_table, result_dict_keys):
     # add the header
-    header = "Temp\tFCC\tBCC\n"
+    header = "Temp\t" + "\t".join(result_dict_keys) + "\n"
     table = header
 
     for x, y in x_table.items():
-        table += f"{x}\t{y['FCC']}\t{y['BCC']}\n"
+        row = f"{x}"
+        for key in result_dict_keys:
+            row += f"\t{y[key]}"
+        row += "\n"
+        table += row
 
     return table
 
 
 if __name__ == "__main__":
-
     fcc = Results([100, 110, 120], [1, 1, 1], "FCC")
     bcc = Results([130, 140, 150], [1, 1, 1], "BCC")
 
     calc_results = {"FCC": fcc, "BCC": bcc}
 
-    print(x_table(calc_results))
+    x_table_result = x_table(calc_results)
 
-    print(format_table(x_table(calc_results)))
+    print(x_table_result)
 
-
-
-
-
-
-
+    print(format_table(x_table_result, calc_results.keys()))
