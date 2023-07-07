1a04d7ac02e11dc22b9665837634de0c3e244a9e
nbeaver <nbeaver@CENG-RDSRV-03>
2023-07-07 16:30:21 -0700

    updates

--- phase_calculation.py
+++ phase_calculation.py
@@ -1,6 +1,7 @@
 from tc_python import *
-
-elements=['Hf', 'Nb']
+import os
+import itertools
+elements=['Hf','Zr']
 
 def calculate_phases(elements):
 	with TCPython() as start:
@@ -18,7 +19,7 @@ def calculate_phases(elements):
 		prop_diag = prop_diag_calc.with_axis(axis)
 
 		# set conditions
-		if len(elements) !=1:
+		if len(elements) > 1:
 			for element in elements[0:-1]:
 				prop_diag.set_condition(ThermodynamicQuantity.mole_fraction_of_a_component(element), 1/len(elements))		 
 
@@ -26,12 +27,34 @@ def calculate_phases(elements):
 		calculation = prop_diag.calculate()
 
 		# get values grouped by stable phases
-		property_diagram = calculation.get_values_grouped_by_stable_phases_of(ThermodynamicQuantity.temperature(), ThermodynamicQuantity.volume_fraction_of_a_phase("ALL"))
+		property_diagram = calculation.get_values_grouped_by_quantity_of(ThermodynamicQuantity.temperature(), ThermodynamicQuantity.mole_fraction_of_a_phase("ALL"), sort_and_merge=False)
 
-		property_diagram.save_to_disk('C:\code\github')
-	# get result quantities
+		return property_diagram
+
 		
+	# get result quantities
+	
+def format_results(property_diagram):
+	
+	temperature=[]
 
-calculate_phases(elements)
+	for key,value in property_diagram.items():
+		x_values = value.get_x()
+		for x in x_values:
+			
 
+
+
+	property_diagram
+	with open('output.txt', 'w') as f:
+		f.write('Key\tValue_X\tValue_Y\n')
+		for key, value in property_diagram.items():
+			x_values = value.get_x()
+			y_values = value.get_y()
+			for x, y in itertools.zip_longest(x_values, y_values):
+				f.write(f'{key}\t{x}\t{y}\n')
+
+
+result = calculate_phases(elements)
+format_results(result)
 
